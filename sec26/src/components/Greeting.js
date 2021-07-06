be24cb76b0f700890af13672370322f09a7f6c02
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2021-07-06 11:55:50 +0800

    add mock function

--- sec26/src/components/Greeting.js
+++ sec26/src/components/Greeting.js
@@ -1,18 +1,19 @@
 import { useState } from 'react';
 
+import Output from './Output';
+
 const Greeting = () => {
-    const [changedText, setChangedText] = useState(false);
+  const [changedText, setChangedText] = useState(false);
 
-    const changeTextHandler = () => {
-        setChangedText(true);
-    };
-
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
 
   return (
     <div>
       <h2>Hello World!</h2>
-      {!changedText && <p>It's good to see you!</p>}
-      {changedText && <p>Changed!</p>}
+      {!changedText && <Output>It's good to see you!</Output>}
+      {changedText && <Output>Changed!</Output>}
       <button onClick={changeTextHandler}>Change Text!</button>
     </div>
   );
